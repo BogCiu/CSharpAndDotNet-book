f8093ed50b32483a711d36d0c19c7179517bd325
Bogdan Voicu <bogdan.bog.voicu@gmail.com>
2023-04-14 01:19:00 +0300

    Ch 17 finished and Ch 18 started

---
+++ Chapter17/BeginningCSharpAndDotNET_17_1_CodeFirstDatabase/BeginningCSharpAndDotNET_17_1_CodeFirstDatabase/Migrations/20230401223343_InitializeThenStoresAndBooks.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeginningCSharpAndDotNET_17_1_CodeFirstDatabase.Migrations
+{
+    public partial class InitializeThenStoresAndBooks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Code = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    StoreID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.StoreID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stocks",
+                columns: table => new
+                {
+                    StockID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OnHand = table.Column<int>(type: "int", nullable: false),
+                    OnOrder = table.Column<int>(type: "int", nullable: false),
+                    ItemCode = table.Column<int>(type: "int", nullable: true),
+                    StoreID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stocks", x => x.StockID);
+                    table.ForeignKey(
+                        name: "FK_Stocks_Books_ItemCode",
+                        column: x => x.ItemCode,
+                        principalTable: "Books",
+                        principalColumn: "Code",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Stocks_Stores_StoreID",
+                        column: x => x.StoreID,
+                        principalTable: "Stores",
+                        principalColumn: "StoreID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stocks_ItemCode",
+                table: "Stocks",
+                column: "ItemCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stocks_StoreID",
+                table: "Stocks",
+                column: "StoreID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stocks");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Stores");
+        }
+    }
+}
