1b12dc1f00bc5cbce7541bbfd025bc7fc0eab9e0
Bogdan Voicu <bogdan.bog.voicu@gmail.com>
2024-11-08 16:39:53 +0200

    Chapter 19 commit

---
+++ Chapter19/Ch19ExExtra19.3/Ch19ExExtra19.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using static System.Console;
+
+namespace Ch19ExExtra19._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cards = new List<string>();
+            var playerName = "Benjamin";
+            string GetURL =
+                "http://handofcards.azurewebsites.net/api/HandOfCards/" + playerName; // Wtf is this supposed to be?
+            WebClient client = new WebClient();
+            Stream dataStream = client.OpenRead(GetURL);
+            StreamReader reader = new StreamReader(dataStream);
+            var results = JsonConvert.DeserializeObject<dynamic>(reader.ReadLine());
+            reader.Close();
+            foreach (var item in results)
+            {
+                WriteLine((string)item.imagelink);
+            }
+            ReadLine();
+        }
+    }
+}
