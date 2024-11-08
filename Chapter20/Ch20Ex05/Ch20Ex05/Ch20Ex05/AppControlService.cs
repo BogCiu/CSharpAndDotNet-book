e822a7cc3bc363c48f084885f92a0389ba244ea0
Voicu, Bogdan <bogdan.bog.voicu@gmail.com>
2024-11-08 16:28:21 +0200

    Chapter 20 commit

---
+++ Chapter20/Ch20Ex05/Ch20Ex05/Ch20Ex05/AppControlService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Ch20Ex05
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AppControlService" in both code and config file together.
+    public class AppControlService : IAppControlService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
