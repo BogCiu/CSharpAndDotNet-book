103585eb62aa5092c37b4321abcc4f9153fc84e8
Bogdan Voicu <bogdan.bog.voicu@gmail.com>
2025-01-04 20:32:44 +0200

    Modified code to use a MinIO instance hosted on my server instead of Azure, cause I'm cheap like that.

--- Chapter19/Ch19Ex01/Ch19Ex01/Program.cs
+++ Chapter19/Ch19Ex01/Ch19Ex01/Program.cs
@@ -1,76 +1,103 @@
-﻿using NextcloudApi;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.InteropServices;
-using System.Xml.Linq;
+﻿using Minio;
+using Minio.DataModel;
+using Minio.DataModel.Args;
+using Minio.Exceptions;
+using Newtonsoft.Json;
+
 namespace Ch19Ex01
 {
-    internal class Program
+    public class Program
     {
-        public static Settings _settings;
-        public static Api _api;
-        static void Main()
+        //public static Settings _settings;
+        //public static Api _api;
+        public static async Task Main()
         {
+            string credentialsRelativePath = @"..\..\..\credentials.json";
+            using StreamReader r = new StreamReader(credentialsRelativePath);
+            string json = r.ReadToEnd();
+            dynamic credentials = JsonConvert.DeserializeObject(json);
+            string accessKey = credentials["accessKey"];
+            string secretKey = credentials["secretKey"];
+
+            string bucketName = "cards";
+            string contentType = "image/png";
+
+            string endpoint = "mioapi.eventifyofficial.com";
+            string region = "eu-east-1";
+
+            var minioClient = new MinioClient()
+                              .WithEndpoint(endpoint)
+                              //.WithCredentials("Book_Test_User", "Book_Test_User")
+                              .WithCredentials(accessKey, secretKey)
+                              .WithRegion(region)
+                              .WithSSL(true)
+                              .Build();
+
             try
             {
-                DirectoryInfo dir = new DirectoryInfo(@"Cards");
-                foreach(FileInfo f in dir.GetFiles("*.*"))
+                //Create bucket if it doesn't exist.
+                BucketExistsArgs bea = new BucketExistsArgs().WithBucket(bucketName);
+                bool found = await minioClient.BucketExistsAsync(bea);
+                Console.WriteLine($"Found buckets? - {found}");
+                if (found)
                 {
-                    using FileStream stream = File.OpenRead(@"Cards\" + f.Name);
-                    {
-                        Task<string> UploadTask = CloudFile.Upload(Api, Settings.Username + $"/C_Sharp_Book/{f.Name}", stream);
-                        string taskResolution = RunTask(UploadTask);
-                        //Console.WriteLine(f.Name);
-                    }
+                    Console.WriteLine($"{bucketName} already exists");
                 }
-
-                Task<List<CloudInfo>> myNextCloudProjectFolderSearch = CloudFolder.List(Api, Settings.Username + "/C_Sharp_Book");
-                List<CloudInfo> MyFolderCI = RunTask(myNextCloudProjectFolderSearch);
-                Console.WriteLine(MyFolderCI.ToHumanReadableJson());
-
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Generic Exception : {ex}");
-            }
-            Console.WriteLine("Press Enter to exit.");
-            Console.ReadLine();
-        }
-        public static T RunTask<T>(Task<T> task)
-        {
-            T t = task.Result;
-            //Console.WriteLine(t);
-            return t;
-        }
-        public static Api Api
-        {
-            get
-            {
-                _api ??= new Api(Settings);
-                return _api;
-            }
-        }
-        public static Settings Settings
-        {
-            get
-            {
-                if (_settings == null)
+                else
                 {
-                    string dataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "NextcloudApi");
-                    Directory.CreateDirectory(dataPath);
-                    string filename = Path.Combine(dataPath, "TestSettings.json"); // C:\Users\Administrator\AppData\Local\NextCloudAPI
-
-                    _settings = new Settings();
-                    _settings.Load(filename);
-                    List<string> errors = _settings.Validate();
-                    if (errors.Count > 0)
-                    {
-                        throw new ApplicationException(string.Join("\r\n", errors));
-                    }
+                    //Create bucket 'cards'.
+                    await minioClient.MakeBucketAsync(new MakeBucketArgs().WithBucket(bucketName));
+                    Console.WriteLine($"{bucketName} is created successfully");
                 }
-                return _settings;
+            }
+            catch (MinioException e)
+            {
+                Console.WriteLine("Error occurred: " + e);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            try
+            {
+                var progress = new Progress<ProgressReport>(progressReport =>
+                {
+                    Console.WriteLine($"Percentage: {progressReport.Percentage}% TotalBytesTransferred: {progressReport.TotalBytesTransferred} bytes");
+                    if (progressReport.Percentage != 100)
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    else Console.WriteLine();
+                });
+
+                string[] files = Directory.GetFiles(@"..\..\..\Cards", "*.png");
+                foreach (string file in files)
+                {
+                    string objectName = Path.GetFileName(file);
+                    string objectPath = "Cards/" + objectName;
+
+                    PutObjectArgs poa = new PutObjectArgs()
+                    .WithBucket(bucketName)
+                    .WithObject(objectName)
+                    .WithFileName(objectPath)
+                    .WithContentType(contentType)
+                    .WithProgress(progress)
+                    ;
+
+                    await minioClient.PutObjectAsync(poa);
+                    //StatObjectArgs soa = new StatObjectArgs()
+                    //    .WithBucket(bucketName)
+                    //    .WithObject(objectName);
+
+                    //ObjectStat state = await minioClient.StatObjectAsync(soa);
+                    //Console.WriteLine(state.ObjectName);
+                }
+            }
+            catch (ObjectNotFoundException onfe)
+            {
+                Console.WriteLine($"Error: {onfe.Message}");
+            }
+            catch (MinioException me)
+            {
+                Console.WriteLine($"Error: {me.Message}");
             }
         }
-
     }
 }
